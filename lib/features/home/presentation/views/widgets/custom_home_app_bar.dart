be72c55e542c60023bef3db239c40d5019d374a0
John Amir <jofahmy8@gmail.com>
2025-03-31 21:14:09 +0200

    created post design, and created post list view done

---
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:nafsia/core/helper_functions/extentions.dart';
+import 'package:nafsia/core/helper_functions/rouutes.dart';
+import 'package:nafsia/core/utils/app_colors.dart';
+import 'package:nafsia/core/utils/app_text_styles.dart';
+
+class CustomHomeAppBar extends StatelessWidget {
+  const CustomHomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text('اهلًا بك مريم 👋',style: TextStyles.bold16.copyWith(color: AppColors.primaryColor),),
+        const Spacer(),
+        GestureDetector(
+          onTap: (){
+            context.pushNamed(Routes.modeSelectionView);
+          },
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(16)
+            ),
+            child: Text('كيف تشعر اليوم ؟ 😀',style: TextStyles.bold13,)),
+        )
+          
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,40 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:nafsia/core/helper_functions/extentions.dart';
-import 'package:nafsia/core/helper_functions/rouutes.dart';
-import 'package:nafsia/core/utils/app_colors.dart';
-import 'package:nafsia/core/utils/app_text_styles.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nafsia/core/utils/spacing.dart';
+import 'package:nafsia/core/widgets/custom_post_item.dart';
+import 'package:nafsia/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 12),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text('اهلًا بك مريم 👋',style: TextStyles.bold16.copyWith(color: AppColors.primaryColor),),
-              Spacer(),
-              GestureDetector(
-                onTap: (){
-                  context.pushNamed(Routes.modeSelectionView);
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.circular(16)
-                  ),
-                  child: Text('كيف تشعر اليوم ؟ 😀',style: TextStyles.bold13,)),
-              )
-      
-            ],
-          )
-        ],
-      ),
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverToBoxAdapter(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0.h, vertical: 12),
+          child: const CustomHomeAppBar(),
+        )),
+        SliverToBoxAdapter(
+          child: verticalSpace(16),
+        ),
+        SliverList.builder(
+          itemBuilder: (context, index) => const CustomPostItem(),
+          itemCount: 10,
+        )
+      ],
     );
   }
 }
